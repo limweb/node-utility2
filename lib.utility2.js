dde45f4c175d27e37f2091845490bfa05119f632
kai zhu <nobody>
2017-05-02 21:02:22 +0800

    fix statusCode

--- lib.utility2.js
+++ lib.utility2.js
@@ -1904,7 +1904,8 @@ local.assetsDict['/favicon.ico'] = '';
             xhr.addEventListener('progress', local.ajaxProgressUpdate);
             xhr.upload.addEventListener('progress', local.ajaxProgressUpdate);
             // open url
-            xhr.open(xhr.method, ((/^https{0,1}:/).test(xhr.url) &&
+            xhr.open(xhr.method, (local.modeJs === 'browser' &&
+                (/^https{0,1}:/).test(xhr.url) &&
                 xhr.url.indexOf(location.protocol + '//' + location.host) !== 0 &&
                 local.env.modeForwardProxyUrl) || xhr.url);
             // set request-headers
@@ -3818,7 +3819,7 @@ return Utf8ArrayToStr(bff);
                 ? local.https
                 : local.http).request(options, function (clientResponse) {
                 options.clientResponse = clientResponse.on('error', onError);
-                response.statusCode = options.clientRespone.statusCode;
+                response.statusCode = options.clientResponse.statusCode;
                 // pipe clientResponse to serverResponse
                 options.clientResponse.pipe(response);
             }).on('error', onError);
